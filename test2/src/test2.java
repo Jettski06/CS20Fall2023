8444063c8a6c980de5fc8d878960f51726474ff6
16116012 <16116012@L815WD220035.edu.cbe.ab.ca>
2023-09-12 09:43:29 -0600

    9:43AM

---
+++ test2/src/test2.java
@@ -0,0 +1,22 @@
+
+public class test2 {
+
+	public static void main(String[] args) 
+	{ 
+		//Declaration
+		int age;
+		
+		//Initialization
+		age = 17;
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.print(age + 3);
+	}
+
+}
---
+++ test2/src/test2/Test.java
@@ -0,0 +1,27 @@
+package test2;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+	//Declaration	
+		int age;
+		double cost;
+		String name;
+		
+		//initialization
+		age = 17;
+		cost = 45.50;
+		name = "Jett";
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
