4451c1021525629d5498234defa325d68996f901
16116012 <16116012@L815WD220035.edu.cbe.ab.ca>
2023-09-13 11:09:05 -0600

    11:08AM

--- test2/src/test2/Test.java
+++ test2/src/test2/Test.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-	
-		System.out.print("welcome to computer science 20/n/n");
-
-		Scanner input = new Scanner(System.in);
-		System.out.print(" Enter your age: ");
-
-		int age = input.nextInt();
 		
-		System.out.print("The age you typed is: " + age);
+		
+		System.out.print("Welcome to computer science 20/n/n");
+		System.out.print("Welcome to room 314/n/n");
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Enter the first number");
+	
+		double num1 = input.nextDouble();
+		
+		System.out.print("Enter your second number");
+		
+		double num2 = input.nextDouble();
+		
+		int sum = (int) (num1 + num2);
+		
 		
 		
 		
@@ -21,4 +29,3 @@ public class Test {
 	}
 
 }
-//IPO
